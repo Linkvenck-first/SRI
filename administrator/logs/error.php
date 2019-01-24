09eee339125717d32753f4d3e5c7482a89939e6c
root <1630045@upv.edu.mx>
2019-01-24 17:31:18 +0000

    rikito instalado limpio

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-01-24 17:21:32 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-01-24T17:21:32+00:00	INFO 189.204.231.185	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-01-24 17:20:49 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-01-24T17:20:48+00:00	INFO 10.9.38.11	update	COM_JOOMLAUPDATE_UPDATE_LOG_DELETE_FILES
